ae0401825aefaa0d9d2e9f0735e2e506e19423e4
Iryna Klosheva <aripluss@i.ua>
2026-02-08 02:42:42 +0200

    Updates

---
+++ assets/product-Beay9unf.js
@@ -0,0 +1,1 @@
+import{l as a,g as t}from"./utils-DYqITUUs.js";async function o(){await a("header",t("header.html")),await a("product-header",t("product-page/product-header.html")),await a("product-info",t("product-page/product-info.html")),await a("footer",t("footer.html"))}o();
